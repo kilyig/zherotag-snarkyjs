d32b9bc2dc26e08651a0000b75d1e37702952f94
kilyig <yigit@kilicoglu.nom.tr>
2022-10-21 09:51:00 -0400

    add modular exponentiation and tests

--- src/Utils.test.ts
+++ src/Utils.test.ts
@@ -10,7 +10,63 @@ describe('modexp function', () => {
     setTimeout(shutdown, 0);
   });
 
-  test('returns 1 (dummy)', () => {
-    expect(modexp(Field(7), Field(15))).toEqual(Field(1));
+  test('exponents of 1', () => {
+    for (let exp: number = 0; exp <= 10; exp++) {
+      expect(modexp(Field(1), exp)).toEqual(Field(1));
+    }
+  });
+
+  test('exponents of 2', () => {
+    expect(modexp(Field(2), 0)).toEqual(Field(1));
+    expect(modexp(Field(2), 1)).toEqual(Field(2));
+    expect(modexp(Field(2), 2)).toEqual(Field(4));
+    expect(modexp(Field(2), 3)).toEqual(Field(8));
+    expect(modexp(Field(2), 4)).toEqual(Field(16));
+    expect(modexp(Field(2), 5)).toEqual(Field(32));
+    expect(modexp(Field(2), 6)).toEqual(Field(64));
+  });
+
+  test('exponents of -1', () => {
+    // even powers are 1
+    for (let exp: number = 0; exp <= 20; exp += 2) {
+      expect(modexp(Field.minusOne, exp)).toEqual(Field(1));
+    }
+
+    // odd powers are -1
+    for (let exp: number = 1; exp <= 21; exp += 2) {
+      expect(modexp(Field.minusOne, exp)).toEqual(Field.minusOne);
+    }
+  });
+
+  test('exponents of -2', () => {
+    expect(modexp(Field(-2), 0)).toEqual(Field(1));
+    expect(modexp(Field(-2), 1)).toEqual(Field(-2));
+    expect(modexp(Field(-2), 2)).toEqual(Field(4));
+    expect(modexp(Field(-2), 3)).toEqual(Field(-8));
+    expect(modexp(Field(-2), 4)).toEqual(Field(16));
+    expect(modexp(Field(-2), 5)).toEqual(Field(-32));
+    expect(modexp(Field(-2), 6)).toEqual(Field(64));
+  });
+
+  test('commutativity', () => {
+    let exp_1 = 3;
+    let exp_2 = 7;
+    let base: Field = Field(5);
+
+    let exp_1_first = modexp(modexp(base, exp_1), exp_2);
+    let exp_2_first = modexp(modexp(base, exp_2), exp_1);
+
+    expect(exp_1_first).toEqual(exp_2_first);
+  });
+
+  test('add exponents then raise = raise to exponents and then multiply', () => {
+    let exp_1 = 3;
+    let exp_2 = 7;
+    let base: Field = Field(5);
+
+    let add_then_raise = modexp(base, exp_1 + exp_2);
+    let raise_then_multiply = modexp(base, exp_1).mul(modexp(base, exp_2));
+
+    expect(add_then_raise).toEqual(raise_then_multiply);
   });
 });
--- src/Utils.ts
+++ src/Utils.ts
@@ -1,7 +1,14 @@
-import { Field } from 'snarkyjs';
+import { Circuit, Field } from 'snarkyjs';
 
-export function modexp(g: Field, x: Field) {
-  console.log(g);
-  console.log(x);
-  return Field(1);
+export function modexp(g: Field, x: number) {
+  let result = Field(1);
+  let base = g;
+
+  while (x > 0) {
+    result = Circuit.if(x % 2 === 1, result.mul(base), result);
+    x = Math.floor(x / 2);
+    base = base.square();
+  }
+
+  return result;
 }
