f6ffb3331af688190cd11589e8aa8d427e2ea76e
kilyig <yigit@kilicoglu.nom.tr>
2022-10-27 16:09:40 -0400

    remove reference to the judge contract for CI

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@ import { PiecePosition } from './PiecePosition.js';
 import { ZheroTagPlayer } from './ZheroTagPlayer.js';
 import { ZheroTagGame } from './ZheroTagGame.js';
 import { ZheroTagProtocolState } from './ZheroTagProtocolState.js';
-import { ZheroTagJudge } from './ZheroTagJudge.js';
 
 export { PiecePosition };
 export { ZheroTagPlayer };
 export { ZheroTagGame };
 export { ZheroTagProtocolState };
-export { ZheroTagJudge };
